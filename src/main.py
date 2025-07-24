8c84ce14a402b39e831355b00ff20d61bf42c02b
Culottes <culottes@sagbot.com>
2025-07-24 09:48:08 +0200

    feat(src/main.py): Wrapping the code in a main function

--- src/main.py
+++ src/main.py
@@ -6,39 +6,47 @@ from util import read_to_string, unwrap, user_prompt
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-database: Database = Neo4j(
-    uri=unwrap(os.getenv("NEO4J_URI")),
-    username=unwrap(os.getenv("NEO4J_USERNAME")),
-    password=unwrap(os.getenv("NEO4J_PASSWORD")),
-    database=unwrap(os.getenv("NEO4J_DATABASE")),
-)
+def main() -> None:
+    load_dotenv()
 
-llm: LLM = Ollama(
-    host=unwrap(os.getenv("OLLAMA_URI")),
-    model="mistral:7b",
-    chat=[
-        Message(role="system", content=read_to_string("./resources/prompt/system.txt"))
-    ],
-    cleaner=lambda s: s[5:-4],
-)
+    database: Database = Neo4j(
+        uri=unwrap(os.getenv("NEO4J_URI")),
+        username=unwrap(os.getenv("NEO4J_USERNAME")),
+        password=unwrap(os.getenv("NEO4J_PASSWORD")),
+        database=unwrap(os.getenv("NEO4J_DATABASE")),
+    )
 
-retrieval: LLM = Graph(llm, database)
+    llm: LLM = Ollama(
+        host=unwrap(os.getenv("OLLAMA_URI")),
+        model="mistral:7b",
+        chat=[
+            Message(
+                role="system", content=read_to_string("./resources/prompt/system.txt")
+            )
+        ],
+        cleaner=lambda s: s[5:-4],
+    )
 
-base = read_to_string("./resources/prompt/user.txt")
+    retrieval: LLM = Graph(llm, database)
 
-result = retrieval.chat(
-    chat=[
-        Message(
-            role="user",
-            content=user_prompt(
-                base,
-                intent="Information request",
-                request="What are the symptoms of asthma?",
-            ),
-        )
-    ]
-)
+    base = read_to_string("./resources/prompt/user.txt")
 
-print("Result:", result)
+    result = retrieval.chat(
+        chat=[
+            Message(
+                role="user",
+                content=user_prompt(
+                    base,
+                    intent="Information request",
+                    request="What are the symptoms of asthma?",
+                ),
+            )
+        ]
+    )
+
+    print("Result:", result)
+
+
+if __name__ == "__main__":
+    main()
